f6f11a17257a20a57797db7e33ea921f0d1fd41a
Samyr <samirgamesmarjoub@gmail.com>
2022-12-28 00:17:49 -0300

    ..

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -30,16 +30,7 @@ export const authOptions = {
             return session
         }
     },
-    // adapter: FirestoreAdapter({
-    //     apiKey: process.env.FIREBASE_API_KEY,
-    //     appId: process.env.FIREBASE_APP_ID,
-    //     authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-    //     databaseURL: process.env.FIREBASE_DATABASE_URL,
-    //     projectId: process.env.FIREBASE_PROJECT_ID,
-    //     storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-    //     messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-    //     emulator: {},
-    // }),
+    secret: process.env.JWT_SECRET
 
 }
 export default NextAuth(authOptions)
